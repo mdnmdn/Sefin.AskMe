cd02756aa0a41d3d2c314450644ae4ca1fc3d5e1
Marco De Nittis <marco.denittis@gmail.com>
2016-06-03 17:17:05 +0200

    survey list

--- Sefin.AskMe.MvcApp/Controllers/HomeController.cs
+++ Sefin.AskMe.MvcApp/Controllers/HomeController.cs
@@ -12,6 +12,6 @@ namespace Sefin.AskMe.MvcApp.Controllers
         {
             return View();
         }
-
+     
     }
 }
---
+++ Sefin.AskMe.MvcApp/Controllers/SurveyManagerController.cs
@@ -0,0 +1,25 @@
+﻿using Sefin.AskMe.Logic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Sefin.AskMe.MvcApp.Controllers
+{
+    public class SurveyManagerController : Controller
+    {
+
+        SurveyServices _serivices = new SurveyServices();
+
+        public ActionResult Index()
+        {
+            List<SurveyInfo> surveyList = _serivices.ListSurveys()
+                                                    .ToList();
+
+            ViewBag.MioSaluto = "Ciao";
+
+            return View(surveyList);
+        }
+    }
+}
