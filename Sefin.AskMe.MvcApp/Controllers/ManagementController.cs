ad21e48dc1dd2c1828490067cc038f3ae0f65d54
Marco De Nittis <marco.denittis@gmail.com>
2016-10-06 17:45:24 +0200

    edit form

--- Sefin.AskMe.MvcApp/Controllers/ManagementController.cs
+++ Sefin.AskMe.MvcApp/Controllers/ManagementController.cs
@@ -10,6 +10,9 @@ namespace Sefin.AskMe.MvcApp.Controllers
 {
     public class ManagementController : Controller
     {
+
+        SurveyServices _service = new SurveyServices();
+
         public ActionResult Index()
         {
             return RedirectToAction("List");
@@ -21,11 +24,42 @@ namespace Sefin.AskMe.MvcApp.Controllers
                 SearchText = searchText
             };
 
-            var svc = new SurveyServices();
-
-            model.SurveyList = svc.ListSurveys(searchText).ToList();
+            model.SurveyList = _service.ListSurveys(searchText).ToList();
 
             return View(model);
         }
+
+        [HttpGet]
+        public ActionResult Edit(string id) {
+
+            var model = _service.GetSurvey(id);
+
+            return View(model);
+        }
+
+
+        [HttpPost ]
+        public ActionResult Edit(SurveyInfo model)
+        {
+            if (model.Name != null && model.Name[0] == 's')
+            {
+                ModelState.AddModelError("", "il nome non deve iniziare per 's'");
+            }
+
+            if (ModelState.IsValid) {
+                var survey = _service.GetSurvey(model.Id);
+
+                survey.Name = model.Name;
+                survey.Description = model.Description;
+
+                _service.SaveSurvey(survey);
+
+                return RedirectToAction("Edit",new { model.Id });
+            }
+
+            return View("Edit", model);
+        }
+
+
     }
 }
